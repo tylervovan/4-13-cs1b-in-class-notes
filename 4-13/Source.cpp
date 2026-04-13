ecab8ce259d064472ee7586f75973f16c99ea76c
idislikekiasouls <25tylerv@gmail.com>
2026-04-13 12:33:46 -0700

    quick commit (still errors)

--- 4-13/Source.cpp
+++ 4-13/Source.cpp
@@ -1,7 +1,5 @@
 #include "Book.h"
 
-void comparePrice(Book& book1, Book& book2); 
-
 int main()
 {
 	Book book1("the Great Man", 15.99);
@@ -21,9 +19,52 @@ int main()
 
 	cout << endl;
 
-	cout << "They are " << /*TODO*/ " Books. \n\n"; 
+	cout << "They are " << Book::getCount() << " Books. \n\n"; 
 
+	if (book1 > book2)
+	{
+		cout << "The price of book1 is greater tahn book2. \n"; 
+	}
 
+	else
+	{
+		cout << "Book2 is more expensive than book1.\n";
+	}
 
+	if (book1 > 1000)
+	{
+		cout << "Book1 is great than $1000.\n";
+	}
+
+	else
+	{
+		cout << "Book2 is less than $2000. \n"; 
+	}
+
+	double avg{}; 
+
+	avg = (book1 + book2) / 2.0;
+
+	cout << "The average book cost is " << avg << endl; 
+
+	cout << "\nPrinting books using operator <<\n";
+
+	cout << "\nBook1:\n" << book1 << "Book2:\n" << book2 << "Book3:\n" << book3 << endl << endl; 
 	return 0; 
+}
+
+void comparePrices(Book& book1, Book& book2)
+{
+	if (book1.cost > book2.cost)
+	{
+		cout << book1.title << " is more expensive than " << book2.title << endl; 
+	}
+	else if (book1.cost < book2.cost)
+	{
+		cout << book1.title << " is more expensive than " << book1.title << endl; 
+	}
+	else
+	{
+		cout << book1.title << " is the same price as " << book2.title << endl; 
+	}
 }
